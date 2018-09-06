b3984a12a9f4db92c56af1252c8b7a664a9338ac
Joachim Tveita <jotv0906@gmail.com>
2018-09-06 12:36:40 +0200

    Lagd metoden ombyttinger

--- AlgDatOblig1/src/Oblig1.java
+++ AlgDatOblig1/src/Oblig1.java
@@ -10,8 +10,9 @@ Gustav Wehn, s326171
 public class Oblig1 {
     
     public static void main(String [] args) {
-        int a[]= {6,5,4,3,2,1};
+        int a[]= {65,93,95,17,80,8};
         System.out.println(maks(a));
+        System.out.println(ombyttinger(a));
     }
     
     //Oppgave 1
@@ -32,5 +33,21 @@ public class Oblig1 {
         }
         System.out.println("Største verdi er:");
         return maksT;
+    }
+    
+    public  static int ombyttinger(int[] a) {
+        int ombyttinger = 0;
+        System.out.println(Arrays.toString(a));
+        for (int i=1; i<a.length; i++) {
+            if (a[i-1]> a[i]) {
+                int tmp = a[i-1];
+                a[i-1] = a[i];
+                a[i] = tmp;
+                System.out.println(Arrays.toString(a));
+                ombyttinger++;
+            }
+        }
+        System.out.println("Antall ombyttinger:");
+        return ombyttinger;
     }
 }
