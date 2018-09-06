fb57fc241107de04337e1799bad9f19a237d3c0c
Joachim Tveita <jotv0906@gmail.com>
2018-09-06 11:16:26 +0200

    Begynt på oppgave 1

--- AlgDatOblig1/src/Oblig1.java
+++ AlgDatOblig1/src/Oblig1.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
  /*
 Gruppemedlemmer:
 Joachim Tveita
@@ -6,5 +9,23 @@ Gustav Wehn
 */
 public class Oblig1 {
     
+    public static void main(String [] args) {
+        int a[]= {6,5,4,3,2,1};
+        System.out.println(maks(a));
+    }
     
+    //Oppgave 1
+    public static int maks(int[] a) {
+        int tmp = 0;
+        int b[] = new int[0];
+        for (int i=1; i<a.length; i++) {
+            if (a[i-1]> a[i]) {
+                tmp = a[i-1];
+                a[i-1] = a[i];
+                a[i] = tmp;
+                b[] = tmp;
+            }
+        }
+        return b[];
+    }
 }
